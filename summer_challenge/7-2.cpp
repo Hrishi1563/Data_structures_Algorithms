6e228f1b06705bcb4be488dba5671ac4e2156683
Hrishikesh Makwana <hrishi1563@gmail.com>
2023-07-14 16:02:41 +0530

    Pizza Shop-2 final code added

---
+++ summer_challenge/7-2.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS                  \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define endl "\n"
+#define MAXN 100001
+#define INF 1e18 + 1
+
+struct Node
+{
+
+    Node *links[26];
+    bool flag = false;
+
+    bool containsKey(char ch)
+    {
+        return links[ch - 'a'] != NULL;
+    }
+    void put(char ch, Node *node)
+    {
+        links[ch - 'a'] = node;
+    }
+    Node *get(char ch)
+    {
+        return links[ch - 'a'];
+    }
+    void setEnd()
+    {
+        flag = true;
+    }
+
+    bool isEnd() { return flag; }
+};
+
+class Trie
+{
+private:
+    Node *root;
+
+public:
+    Trie()
+    {
+
+        root = new Node();
+    }
+    void insert(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < (int)word.length(); i++)
+        {
+            if (!node->containsKey(word[i]))
+            {
+                node->put(word[i], new Node());
+            }
+
+            node = node->get(word[i]);
+            node->setEnd();
+        }
+    }
+
+    bool search(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < (int)word.length(); i++)
+        {
+            if (!node->containsKey(word[i]))
+            {
+                return false;
+            }
+            node = node->get(word[i]);
+        }
+
+        return node->isEnd();
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Trie trie;
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            string s;
+            cin >> s;
+            bool flag1 = false;
+            bool has_printed = false;
+            for (int j = 1; j <= (int)s.length(); j++)
+            {
+                if (!trie.search(s.substr(0, j)))
+                {
+
+                    if (flag1)
+                        cout << s.size() - (j - 1) + 1 << endl;
+                    else
+                        cout << s.size() << endl;
+
+                    has_printed = true;
+                    break;
+                }
+                flag1 = true;
+            }
+            if (!has_printed)
+                cout << 1 << endl;
+            trie.insert(s);
+        }
+    }
+    return 0;
+}
